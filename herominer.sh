2ef57f68640e410fd162bdda049c743b96cf6466
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2023-12-19 10:03:05 +0700

    Update herominer.sh

--- herominer.sh
+++ herominer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build/xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHYR2bcKrQTbMd1wJBNtctRCkd8LfZ7bpPreRD1zCJiMyY5mJFrqHEoRFwzYL5VKaznpakGsvYTHNSNXztKusvi6N4m6ue41R34 -p node -a rx/0 -k -t 6
+./build/xmrig -o sg.zephyr.herominers.com:1123 -u ZEPHYR2bcKrQTbMd1wJBNtctRCkd8LfZ7bpPreRD1zCJiMyY5mJFrqHEoRFwzYL5VKaznpakGsvYTHNSNXztKusvi6N4m6ue41R34 -p node -a rx/0 -k -t 6
