7e3bd87fd708100bad02c2972f616ffb95b168f7
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2023-12-19 09:23:31 +0700

    Create herominer.sh

---
+++ herominer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./build/xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHYR2bcKrQTbMd1wJBNtctRCkd8LfZ7bpPreRD1zCJiMyY5mJFrqHEoRFwzYL5VKaznpakGsvYTHNSNXztKusvi6N4m6ue41R34 -p node -a rx/0 -k -t 6
